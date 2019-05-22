64b622d5a3f36fcf5e0a6b8c36027fca7994eb8e
Abraham Arce <abraham.arce.moreno@intel.com>
2019-05-21 22:59:23 -0500

    Video Base
    Video Base
    
    Signed-off-by: Abraham Arce <abraham.arce.moreno@intel.com>

--- applications/techie/spacex.sh
+++ applications/techie/spacex.sh
@@ -19,3 +19,7 @@
 . /services/main.sh
 
 /services/image/eog.sh /config/www/media/DiaDeMuertos/Image/Calaverita.jpg &
+sleep 2
+/services/voice/voice.sh 'on' 'espeak' 'spanish' 'Feliz dia de los muertos!'
+sleep 2
+/services/voice/voice.sh 'on' 'espeak' 'english' 'Hi There! My name is Lupe'
