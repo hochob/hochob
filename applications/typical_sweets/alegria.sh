f91c6c4f10c3a8c63c4b6643c19853e893653f09
Yael Castro <cyaelcc94@gmail.com>
2020-05-18 22:04:10 -0500

    Adding two welcome from hochob.backup In order to migrate functionalities from hochob.backup and avoiding repetitive ones, a new structure will be given (For more info refer to migration guide) based in mexican sweets

---
+++ applications/typical_sweets/alegria.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. /services/main.sh
+
+export ALEGRIA_PID=$$
+export ALEGRIA_PATH=$(readlink -f "$0")
+export ALEGRIA_DIRECTORY=$(dirname "$MECCANOID_PATH")
+
+LOCAL_PHRASE="Bienvenidos a mi casa, su casa. \
+     Es un gusto conocerlas y conocerlos. Soy Lupe, \
+     tengo  2 años robot y estudio el primer año de primaria robot. \
+     Mis pasatiempos favoritos son compartir tiempo con mi familia, jugar al aire libre, \
+     leer cuentos divertidos, hacer ejercicio y aprender con mis familiares y amigos \
+     todo el conocimiento científico y tecnológico que está presente en nuestra vida cotidiana. \
+     También soy guía en el emocionante Centro de Visitantes de Intel. \
+     Estoy listo para mostrarles \
+     lo interesante que es el mundo de las Ciencias, la Tecnología, \
+     las Ingenierías y las Matemáticas. A nombre de cada uno de los de los grandes \
+     seres humanos y robots que aquí trabajan les deseamos la mejor de las experiencias. \
+     Muchas gracias por haber venido. \
+    "
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+/services/voice/voice.sh "on" "espeak" "spanish" "$LOCAL_PHRASE"
+
+# End of File
---
+++ applications/typical_sweets/alfenique.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. /services/main.sh
+
+export ALFENIQUE_PID=$$
+export ALFENIQUE_PATH=$(readlink -f "$0")
+export ALFENIQUE_DIRECTORY=$(dirname "$MECCANOID_PATH")
+
+LOCAL_PHRASE="Bienvenidos a mi casa, su casa. \
+    Es un gusto conocerlas y conocerlos. Soy Lupe, \
+    Soy guía en el emocionante Centro de Visitantes de Intel. \
+    Estoy listo para mostrarles lo interesante que es el mundo\
+    de las Ciencias, la Tecnología, las Ingenierías y las Matemáticas.\
+    Por favor pasen, mi voz los guiara a traves de todo el recorrido \
+    "
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+/services/voice/voice.sh "on" "espeak" "spanish" "$LOCAL_PHRASE"
+
+# End of File
