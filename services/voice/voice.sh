5ce1c7c34a5cea7333af43354b5c881453badc9e
Yael Castro <cyaelcc94@gmail.com>
2020-05-28 11:55:10 -0500

    Added text to speech stream to chromecast

--- services/voice/voice.sh
+++ services/voice/voice.sh
@@ -10,6 +10,7 @@ set -x
 
 export SPEECHSYNTHETIZER_PID=$$
 
+
 LOCAL_PLAY="$1"
 LOCAL_ENGINE="$2"
 LOCAL_LANGUAGE="$3"
@@ -33,7 +34,11 @@ if [ $# -eq 4 ]; then
 
     if [ "$LOCAL_PLAY" = "on" ]; then
         aplay $SPEECH_FILE
+    #Stream to chromecast passing the device name in the local_play variable
+    else
+        $CHROMECAST_STREAM "$LOCAL_PLAY" "1.0" "$SPEECH_FILE"
     fi
+
 
 else
     echo "Invalid number of arguments, see Documentation"
