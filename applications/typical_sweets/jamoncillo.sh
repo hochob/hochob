1e592409c35c1bee87bad93758ef78ecc7554483
Yael Castro <cyaelcc94@gmail.com>
2020-05-31 22:50:43 -0500

    Added beginning of second hall

---
+++ applications/typical_sweets/jamoncillo.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. /services/main.sh
+
+export JAMONCILLO_PID=$$
+export JAMONCILLO_PATH=$(readlink -f "$0")
+export JAMONCILLO_DIRECTORY=$(dirname "$MECCANOID_PATH")
+
+LOCAL_PHRASE=" Esta siguiente sección, como lo podrán ver, es bastante diferente a la anterior, \
+             incluso el piso es diferente  \
+            "
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+/services/voice/voice.sh "Lobby TV" "espeak" "spanish" "$LOCAL_PHRASE"
+
+# End of File
