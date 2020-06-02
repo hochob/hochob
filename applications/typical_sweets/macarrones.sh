c8825dabe0a310a6930ce2eabe9c35a085eb333d
Yael Castro <cyaelcc94@gmail.com>
2020-06-01 20:46:54 -0500

    Add "From sand to silicon" voice introduction

---
+++ applications/typical_sweets/macarrones.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. /services/main.sh
+
+export MACARRONES_PID=$$
+export MACARRONES_PATH=$(readlink -f "$0")
+export MACARRONES_DIRECTORY=$(dirname "$MECCANOID_PATH")
+
+LOCAL_PHRASE=" Uno de los elementos clave para fabricar los microprocesadores es el silicio, \
+            y este proviene de la arena, increíble ¿no? \
+            "
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+/services/voice/voice.sh "Lobby TV" "espeak" "spanish" "$LOCAL_PHRASE"
+
+# End of File
