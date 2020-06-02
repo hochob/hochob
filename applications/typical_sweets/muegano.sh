f616ac81b060f400a46db9f7cfcbe8a8710008a1
Yael Castro <cyaelcc94@gmail.com>
2020-06-01 22:51:38 -0500

    Add second hall voice closure

---
+++ applications/typical_sweets/muegano.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. /services/main.sh
+
+export MUEGANO_PID=$$
+export MUEGANO_PATH=$(readlink -f "$0")
+export MUEGANO_DIRECTORY=$(dirname "$MECCANOID_PATH")
+
+LOCAL_PHRASE=" Es todo lo que tengo que contarles acerca de los labs y las fab, \
+            Espero que se hayan tomado fotos
+            "
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+/services/voice/voice.sh "Lobby TV" "espeak" "spanish" "$LOCAL_PHRASE"
+
+# End of File
