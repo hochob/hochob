98be0d607180c428d4ff09966e293d25350d6e7d
Yael Castro <cyaelcc94@gmail.com>
2020-06-02 23:01:36 -0500

    Add third hall voice closure

---
+++ applications/typical_sweets/olladetamarindo.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. /services/main.sh
+
+export OLLA_DE_TAMARINDO_PID=$$
+export OLLA_DE_TAMARINDO_PATH=$(readlink -f "$0")
+export OLLA_DE_TAMARINDO_DIRECTORY=$(dirname "$MECCANOID_PATH")
+
+LOCAL_PHRASE=" Espero que se hayan divertido, nuestro recorrido casi ha terminado. \
+            Los invito a pasar al último pasillo, y en nombre de todos los ingenieros \
+            y robots que trabajamos aquí, les doy las gracias. 
+            "
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+/services/voice/voice.sh "Lobby TV" "espeak" "spanish" "$LOCAL_PHRASE"
+
+# End of File
