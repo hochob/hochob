40a231405e8b26b02664ac63ce6529e2781f2cbf
Yael Castro <cyaelcc94@gmail.com>
2020-06-02 22:04:42 -0500

    Add third hall voice introduction

---
+++ applications/typical_sweets/palanqueta.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. /services/main.sh
+
+export MUEGANO_PID=$$
+export MUEGANO_PATH=$(readlink -f "$0")
+export MUEGANO_DIRECTORY=$(dirname "$MECCANOID_PATH")
+
+LOCAL_PHRASE=" Ahora pasamos a la parte más divertida del recorrido, en esta sección podrán jugar con la ciencia. \
+            Descubrirán un poco más acerca de lo que hacemos aquí. Diviértanse y no olviden preguntar
+            "
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+/services/voice/voice.sh "Lobby TV" "espeak" "spanish" "$LOCAL_PHRASE"
+
+# End of File
