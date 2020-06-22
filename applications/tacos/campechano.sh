2f69a563582b316ec7532edb8fcc2eef0afb3140
Yael Castro <cyaelcc94@gmail.com>
2020-06-22 15:11:48 -0500

    Create Taco Campechano automation, which listens Rhasspy AIUso event
    The Taco Campechano automation return the usage of AI, and how it helps people

---
+++ applications/tacos/campechano.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. /services/main.sh
+
+export CAMPECHANO_PID=$$
+
+LOCAL_PHRASE="Con la inteligencia artificial, las computadoras ayudan a las personas a hacer cosas más rápido y mejor. \
+            Tambien hace a las computadoras ver, hablar, o escuchar, así como lo hago yo.'
+            "
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+/services/voice/voice.sh "amikoo" "default" "spanish" "$LOCAL_PHRASE"
+
+# End of File
