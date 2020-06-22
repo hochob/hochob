8fcd78774f6713185b5ef4a417bffb68c7746da0
Yael Castro <cyaelcc94@gmail.com>
2020-06-22 15:52:45 -0500

    Create Taco de Suadero automation, which listens Rhasspy Despedida event
    The Taco de Suadero automation return the a goodbye phrase

---
+++ applications/tacos/suadero.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. /services/main.sh
+
+export SUADERO_PID=$$
+
+LOCAL_PHRASE[0]="Que la fuerza los acompañe" 
+LOCAL_PHRASE[1]="Hasta luego cocodrilo" 
+LOCAL_PHRASE[2]="Hasta la vista, beibi"
+
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+choosePhrase()
+{
+    rand=$[ $RANDOM % 2 ]
+    LOCAL_PHRASE=${LOCAL_PHRASE[$rand]}
+}
+
+choosePhrase
+
+/services/voice/voice.sh "amikoo" "default" "spanish" "$LOCAL_PHRASE"
+
+# End of File
