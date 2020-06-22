edc37cdb3176b607934c78251a321d77d268bbd1
Yael Castro <cyaelcc94@gmail.com>
2020-06-22 15:00:13 -0500

    Create Taco de Cecina automation, which listens Rhasspy AI event
    This automation will return a simple AI definition

---
+++ applications/tacos/cecina.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. /services/main.sh
+
+export CECINA_PID=$$
+
+LOCAL_PHRASE="Es llevar un pedazo de la inteligencia de las personas a las computadoras, usando matemáticas. \
+             Y en algunos casos es más facil de lo que creen' \
+            "
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+/services/voice/voice.sh "amikoo" "default" "spanish" "$LOCAL_PHRASE"
+
+# End of File
