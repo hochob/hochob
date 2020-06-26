e7ee90fd981e5f24e8186c957c27b82312540282
Yael Castro <cyaelcc94@gmail.com>
2020-06-25 23:38:13 -0500

    Create Taco al Pastor automation, which listens Rhasspy CentroDeVisitantes event
    The Taco al Pastor automation return a brief description about the Visitors Center
    content

---
+++ applications/tacos/pastor.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. /services/main.sh
+
+export PASTOR_PID=$$
+
+LOCAL_PHRASE="Aquí conocerán el pasado, el presente y el futuro de Intel. \
+            Al final encontrarán una sección donde podrán jugar y maravillarse \
+            con la ciencia. Tambien encontrarán un piano cósmico. \
+            "
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+/services/voice/voice.sh "amikoo" "default" "spanish" "$LOCAL_PHRASE"
+
+# End of File
