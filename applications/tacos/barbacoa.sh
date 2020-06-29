352f39764bb9ded15be551ba1b3b934694772bc3
Yael Castro <cyaelcc94@gmail.com>
2020-06-29 13:50:55 -0500

    Create Taco de Barbacoa automation, which listens Rhasspy Muon event
    The Taco de Barbacoa automation return a brief description about what a Muon is
    and its usage

---
+++ applications/tacos/barbacoa.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. /services/main.sh
+
+export BARBACOA_PID=$$
+
+LOCAL_PHRASE=" Los muones son partículas elementales que se encuentran en los rayos \
+    Cósmicos, tienen un tiempo de vida de 2,2 microsegundos. Han sido utilizados para \
+    conocer a detalle volcanes o cámaras ocultas en pirámides egípcias.
+    "
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+/services/voice/voice.sh "amikoo" "default" "spanish" "$LOCAL_PHRASE"
+
+# End of File
