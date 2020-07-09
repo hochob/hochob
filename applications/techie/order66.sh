b97dd97b12e7fb04b7fdaa685036dee7dfaca0f2
Yael Castro <cyaelcc94@gmail.com>
2020-07-08 23:51:26 -0500

    Add support to other voice outputs in Order66 Script
    This script will send a predefined phrased to test the audio
    outputs: Hochob Host, HeyAmikoo and Chromecast

--- applications/techie/order66.sh
+++ applications/techie/order66.sh
@@ -21,6 +21,10 @@ LOCAL_PHRASE="Ejecutando la orden 66
 # Main
 # =============================================================================
 
+/services/voice/voice.sh "on" "espeak" "spanish" "$LOCAL_PHRASE"
+
 /services/voice/voice.sh "amikoo" "default" "spanish" "$LOCAL_PHRASE"
+
+/services/voice/voice.sh "Lobby TV" "espeak" "spanish" "$LOCAL_PHRASE"
 
 # End of File
