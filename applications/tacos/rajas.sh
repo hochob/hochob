f99e14951e65c686f91add3e35b79ed571939178
Yael Castro <cyaelcc94@gmail.com>
2020-07-12 23:09:59 -0500

    Create Taco de Rajas automation, which listens Rhasspy Sustentabilidad event
    The Taco de Rajas automation return a description about how GDC helps the environment,
    and display pictures to help the explanation. This automation is listed
    in the Amikoo tab in the main screen

---
+++ applications/tacos/rajas.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. /services/main.sh
+
+export RAJAS_PID=$$
+
+LOCAL_PHRASE="Estos edificios cuentan con una certificación LEEED Gold.
+            Lo que significa que tomamos medidas para cuidar el ambiente. \
+            Por ejemplo, tenemos una planta de tratamiento de agua, la basura es separada \
+            y reciclada. Además, apoyamos transportes alternativos como el transporte \
+            público y las bicicletas. Tambien se están implementando páneles solares. \
+            "
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+/services/image/feh.sh "/config/www/media/HeyAmikoo/Sustentabilidad" &
+
+/services/voice/voice.sh "amikoo" "default" "spanish" "$LOCAL_PHRASE"
+
+# End of File
