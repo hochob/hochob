b77f0d10bd2d4d59b4bbc811f31da1e082e748be
Yael Castro <cyaelcc94@gmail.com>
2021-05-15 14:01:01 -0500

    Create shell script file to clone media traditions

---
+++ setup.sh
@@ -0,0 +1,16 @@
+mkdir media
+cd media
+git clone https://github.com/hochob/Amikoo.git
+git clone https://github.com/hochob/Briko.git
+git clone https://github.com/hochob/CarasYGestos.git
+git clone https://github.com/hochob/DiaDeMuertos.git
+git clone https://github.com/hochob/Drones.git
+git clone https://github.com/hochob/HeyAmikoo.git
+git clone https://github.com/hochob/Intel.git
+git clone https://github.com/hochob/IntelGuadalajara.git
+git clone https://github.com/hochob/Loteria.git
+git clone https://github.com/hochob/LoteriaMexicana.git
+git clone https://github.com/hochob/Navidad.git
+git clone https://github.com/hochob/Papirolas.git
+git clone https://github.com/hochob/Unidos.git
+git clone https://github.com/hochob/VisitorsCenter.git
